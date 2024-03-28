e0ee8afd008e340b32bf260fb50a8eeeb9ac8ba1
A. Q. Vassopoli <imagingrelieving633@account.vassopoli.com>
2024-03-28 10:26:42 -0300

    chore: add have.ordered.members chai assertion

--- cypress/e2e/jsonpath-dynamic-validation/facts-public-api.cy.js
+++ cypress/e2e/jsonpath-dynamic-validation/facts-public-api.cy.js
@@ -1,6 +1,7 @@
 const validationList = [
     { customJPQuery: '$..source', customAssertion: 'have.length', customValue: 5 },
     { customJPQuery: '$..[?(@.used == true)]._id', customAssertion: 'have.any', customValue: '5887e1d85c873e0011036889' },
+    { customJPQuery: '$.._id', customAssertion: 'have.ordered.members', customValue: JSON.parse('["58e00b5f0aac31001185ed24", "5887e1d85c873e0011036889", "58e008780aac31001185ed05", "58e009390aac31001185ed10", "58e00af60aac31001185ed1d"]') },
     { customJPQuery: '$..source', customAssertion: 'deep.eq', customValue: JSON.parse('["user", "user", "user", "user", "user"]') },
 ]
 
